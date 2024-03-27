bc6954957c1c5165eee581c8ada3fead314f2ff2
Said-Khamis <saidhamic@gmail.com>
2024-03-27 15:17:57 +0300

    load migration database path

--- src/AuthUIServiceProvider.php
+++ src/AuthUIServiceProvider.php
@@ -40,11 +40,11 @@ class AuthUIServiceProvider extends ServiceProvider
 
         $this->publishes([__DIR__.'/../public' => base_path('public')], 'public');
 
-        $this->loadMigrationsFrom(__DIR__.'../database/migrations','database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'../database/migrations');
 
-        /*$this->publishesMigrations([
+        $this->publishesMigrations([
             __DIR__.'/../database/migrations' => database_path('migrations'),
-        ]);*/
+        ]);
 
     }
 }
