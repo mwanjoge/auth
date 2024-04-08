5db7eaba676d4e03e6ddb24e2f4187396e41622a
Said-Khamis <saidhamic@gmail.com>
2024-04-08 12:18:06 +0300

    public all nisimpom modules permissions

--- database/migrations/2100_04_02_120050_create_permission_tables.php
+++ database/migrations/2100_04_02_120050_create_permission_tables.php
@@ -28,7 +28,7 @@ return new class extends Migration
             $table->bigIncrements('id'); // permission id
             $table->string('name');       // For MySQL 8.0 use string('name', 125);
             $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
-            $table->bigInteger('module_id');
+            $table->bigInteger('module_id'); // For MySQL 8.0 use string('guard_name', 125);
             $table->timestamps();
 
             $table->unique(['name', 'guard_name']);
